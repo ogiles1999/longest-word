aa8419c2fce90c591119b6fb2ffe1ea05fdbe693
Oliver Giles <ogiles@email.com>
2022-10-12 12:28:14 +0100

    made compatiable with poetry structure

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,7 +1,7 @@
 # tests/test_game.py
 import unittest
 import string
-from game import Game
+from longest_word.game import Game
 
 class TestGame(unittest.TestCase):
     def test_game_initialization(self):
